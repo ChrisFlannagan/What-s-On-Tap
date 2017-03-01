ab86096e5b6606c0d8306f33e5a900fb3ac8d219
Christopher Flannagan <chris@flowpress.com>
2017-03-01 08:06:05 -0500

    What's on tap readme update

--- whatsontap.php
+++ whatsontap.php
@@ -3,6 +3,9 @@
  * Plugin Name: What's On Tap?
  * Plugin URI: https://whoischris.com
  * Author: Chris Flannagan
+ * Version: 0.1
+ *
+ * Description: Display "What's on Tap" at a bar or restaurant.
  */
 
 require 'includes/constants.php';
